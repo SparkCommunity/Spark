3472fe4b6b21c9ea99ba136d96a9e002cb2d8926
RentaMouse <RentaMouse@users.noreply.github.com>
2014-10-30 01:38:32 +0000

    Update kernel.cpp
    Added PoS stake modifier hard checkpoint for block 10042

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -18,6 +18,7 @@ typedef std::map<int, unsigned int> MapModifierCheckpoints;
 static std::map<int, unsigned int> mapStakeModifierCheckpoints =
     boost::assign::map_list_of
         ( 0, 0xfd11f4e7 )
+        ( 10042, 0x1f05762d)
     ;
 
 // Hard checkpoints of stake modifiers to ensure they are deterministic (testNet)
